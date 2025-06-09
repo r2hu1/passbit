9e6f066742ce4a7739db7958febdf2e3f456aeeb
r2hu1 <hi@rahul.eu.org>
2025-06-09 19:48:45 +0530

    feat: more secure enc and dec using crypto

--- server/utils/crypto.ts
+++ server/utils/crypto.ts
@@ -1,22 +1,39 @@
-import dotenv from "dotenv";
-import {
-  encrypt as scEncrypt,
-  decrypt as scDecrypt,
-} from "@tsmx/string-crypto";
+import * as crypto from "crypto";
 import bcrypt from "bcryptjs";
-dotenv.config();
 
-const encryptionKey = process.env.ENCRYPTION_KEY;
+const encryptionKey = Buffer.from(process.env.ENCRYPTION_KEY, "hex");
 if (!encryptionKey) {
   throw new Error("Encryption key is not set");
 }
 
-export const encrypt = (data: string): string => {
-  return scEncrypt(data, { key: encryptionKey });
+export const encrypt = (plaintext: string): string => {
+  const iv = crypto.randomBytes(12);
+  const cipher = crypto.createCipheriv("aes-256-gcm", encryptionKey, iv);
+
+  const encrypted = Buffer.concat([
+    cipher.update(plaintext, "utf8"),
+    cipher.final(),
+  ]);
+  const tag = cipher.getAuthTag();
+
+  return Buffer.concat([iv, tag, encrypted]).toString("base64");
 };
 
 export const decrypt = (data: string): string => {
-  return scDecrypt(data, { key: encryptionKey });
+  const bData = Buffer.from(data, "base64");
+  const iv = bData.slice(0, 12);
+  const tag = bData.slice(12, 28);
+  const encrypted = bData.slice(28);
+
+  const decipher = crypto.createDecipheriv("aes-256-gcm", encryptionKey, iv);
+  decipher.setAuthTag(tag);
+
+  const decrypted = Buffer.concat([
+    decipher.update(encrypted),
+    decipher.final(),
+  ]);
+
+  return decrypted.toString("utf8");
 };
 
 export const hashPassword = async (password: string): Promise<string> => {
