d2bfae2a12dabbe9e6cbf1499d12aa85a44f5119
r2hu1 <hi@rahul.eu.org>
2025-06-10 23:24:34 +0530

    feat: cors (temp allowing all request)

---
+++ server/middleware/cors.ts
@@ -0,0 +1,22 @@
+import {
+  defineEventHandler,
+  getMethod,
+  getRequestHeader,
+  setResponseHeaders,
+} from "h3";
+
+export default defineEventHandler((event) => {
+  const origin = getRequestHeader(event, "origin") || "*";
+
+  setResponseHeaders(event, {
+    "Access-Control-Allow-Origin": origin,
+    "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    "Access-Control-Allow-Credentials": "true",
+  });
+  console.log("[CORS] headers set");
+  if (getMethod(event) === "OPTIONS") {
+    event.node.res.statusCode = 204;
+    event.node.res.end();
+  }
+});
